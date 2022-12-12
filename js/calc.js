10303b84b322567a3dc3b1c876422e4eae383c34
SpoonsisaBoss <jonathan.edery@hotmail.com>
2022-12-12 13:46:31 -0500

    Initial commit

---
+++ js/calc.js
@@ -0,0 +1,23 @@
+//KNAV
+//keyword, name, assignment, value
+
+let result = document.getElementById('result'); 
+
+//KNAPC
+function addNum(){
+    let firstNum = 2;
+    let secondNum = 2;
+    result.innerHTML = firstNum + secondNum;
+}
+
+function subNum(){
+    let firstNum = 4;
+    let secondNum = 2;
+    result.innerHTML = firstNum - secondNum; 
+}
+
+function mulNum(){
+    let firstNum = 4;
+    let secondNum = 2;
+    result.innerHTML = firstNum * secondNum; 
+}
